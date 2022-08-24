fc7dc412c3ff73a7707949829c3146c8534be671
Alexandru Petru <alexandrup.pirtac@gmail.com>
2022-08-24 15:55:20 +0100

    Set files, get user input and generate random number

---
+++ app.js
@@ -0,0 +1,10 @@
+// Get user input via prompt
+// const userInput = prompt("Rock\nPaper\nScissors\nShoot =>").toLowerCase().trim()
+// console.log(userInput);
+// Generate random number
+const randomNumber = Math.floor(Math.random() * 3);
+console.log(randomNumber)
+// 0 - rock
+// 1 - paper
+// 2 - scissors
+// Appropriate messages
