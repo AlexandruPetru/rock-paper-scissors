642f595c84ddaab05574095588d071541314a4b0
Alexandru Petru <alexandrup.pirtac@gmail.com>
2022-08-24 16:19:34 +0100

    Add logic and implemente messages for each situation

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
 // Get user input via prompt
-// const userInput = prompt("Rock\nPaper\nScissors\nShoot =>").toLowerCase().trim()
+const userInput = prompt("Rock\nPaper\nScissors\nShoot =>").toLowerCase().trim()
 // console.log(userInput);
 // Generate random number
 const randomNumber = Math.floor(Math.random() * 3);
-console.log(randomNumber)
+// console.log(randomNumber)
 // 0 - rock
 // 1 - paper
 // 2 - scissors
 // Appropriate messages
+if(randomNumber === 0) {
+    console.log("Rock");
+    if(userInput === "rock") {
+        console.log("It is a draw. :|")
+    }
+    if(userInput === "paper") {
+        console.log("You've won! :)");
+    }if (userInput === "scissors") {
+        console.log("You lost! :(")
+    }
+} else if (randomNumber === 1) {
+    console.log("Paper")
+    if(userInput === "rock") {
+        console.log("You lost! :(")
+    }
+    if(userInput === "paper") {
+        console.log("It is a draw. :|")
+    }if (userInput === "scissors") {
+        console.log("You've won! :)");
+    }
+} else {
+    console.log("Scissors")
+    if(userInput === "rock") {
+        console.log("You've won! :)");
+    }
+    if(userInput === "paper") {
+        console.log("You lost! :(")
+    }if (userInput === "scissors") {
+        console.log("It is a draw. :|")
+    }
+}
